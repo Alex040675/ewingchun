53cada0191c6f5e20e677bfc7cc3d643a9a8019b
eugene <evgeniy.vozniuk@gmail.com>
2016-11-11 21:33:47 +0200

    sifu template

--- sites/all/themes/ewingchun/templates/node--resource.tpl.php
+++ sites/all/themes/ewingchun/templates/node--resource.tpl.php
@@ -102,7 +102,7 @@
 <div class="school-moreinfo">
 <h5>more info:</h5><br />
 <div class="content">
-  <?php print $node; ?>
+  <?php print_r($node); ?>
 <?php print strip_tags($node->body['und'][0]['value'],"<p>,<br>"); ?>
 </div>
 </div>
