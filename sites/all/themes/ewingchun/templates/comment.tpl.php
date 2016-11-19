fd201e95bed75b1d66ad30e290b0280e95a1b8ad
eugene <evgeniy.vozniuk@gmail.com>
2016-11-19 23:00:22 +0200

    school template

--- sites/all/themes/ewingchun/templates/comment.tpl.php
+++ sites/all/themes/ewingchun/templates/comment.tpl.php
@@ -70,8 +70,14 @@
 
 
         $user = user_load($comment->uid);
-        $url = image_style_url('related_articles', $user->picture->uri);
-        print '<img class="cert" src="'. $url . '" alt="' . $user->name . '" />'
+        if (isset($user->picture->uri)) {
+          $url = image_style_url('user_comment', $user->picture->uri);
+          print '<img class="cert" src="'. $url . '" alt="' . $user->name . '" />'
+        }
+        else {
+          echo variable_get('user_picture_default');
+        }
+
 
       ?></div>
     <div class="centerright">
