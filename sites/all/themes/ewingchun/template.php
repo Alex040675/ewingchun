1d5027bfcdea0d82f20bd765b63593edf46bd12f
eugene <evgeniy.vozniuk@gmail.com>
2016-12-04 22:18:50 +0200

    head instructor

--- sites/all/themes/ewingchun/template.php
+++ sites/all/themes/ewingchun/template.php
@@ -191,8 +191,7 @@ function ewingchun_preprocess_node(&$variables) {
   // Add variables for schools content type.
   if ($variables['node']->type == 'resource') {
     if (!empty($variables['node']->field_instructors['und'][0]['nid'])) {
-      $sifu_node = node_load($variables['node']->field_instructors[0]['nid']);
-      print_r($sifu_node);
+      $sifu_node = node_load($variables['node']->field_instructors['und'][0]['nid']);
       // Pull in linked name
       $sifu_name = l($sifu_node->title, 'node/' . $sifu_node->nid);
       $variables['head_instructor'] = $sifu_name;
