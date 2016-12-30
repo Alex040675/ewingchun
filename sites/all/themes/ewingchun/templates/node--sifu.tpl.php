3f2bd7cc5c2c47384c8bd2462393632cb5b96d42
eugene <evgeniy.vozniuk@gmail.com>
2016-12-30 15:55:49 +0200

    fixing sifu comments

--- sites/all/themes/ewingchun/templates/node--sifu.tpl.php
+++ sites/all/themes/ewingchun/templates/node--sifu.tpl.php
@@ -146,10 +146,9 @@
   <?php endif; ?>
 
 <?php
-if ($logged_in) {
   $comments = comment_node_page_additions($node);
   print render($comments);
 //    $comment_form = drupal_get_form('comment_node_blog_form', (object) array('nid' => $node->nid));
 //    print drupal_render($comment_form);
-}
+
 ?>
