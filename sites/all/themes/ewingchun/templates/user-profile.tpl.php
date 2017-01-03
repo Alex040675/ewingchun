7cc1967593617d30fc4bad8671c75457c2b444e3
eugene <evgeniy.vozniuk@gmail.com>
2017-01-03 14:40:14 +0200

    fixing comments

--- sites/all/themes/ewingchun/templates/user-profile.tpl.php
+++ sites/all/themes/ewingchun/templates/user-profile.tpl.php
@@ -131,8 +131,8 @@ if ($variables['user_profile']['summary']['member_for']['#markup']) {
 </div>
 <div class="blog-part">
 <div class="blog-part-left">
-<h3>topics i'm participating in</h3>
-<?php $abc = views_embed_view('Blogs', 'block_3', $arg);
+<h3>my articles</h3>
+<?php $abc = views_embed_view('Blogs', 'block_3', $uid);
 print $abc; ?>
 </div>
 <div class="blog-part-right">
