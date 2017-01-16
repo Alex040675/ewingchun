7e49f6b29bda1b21ce9cefaffc517cada6a360bd
vitaliy <vetaliy80@mail.ru>
2017-01-16 17:11:46 +0200

    blogs page fixing

--- sites/all/themes/ewingchun/js/ewingchun.behaviors.js
+++ sites/all/themes/ewingchun/js/ewingchun.behaviors.js
@@ -58,8 +58,8 @@
             $('.l-region--navigation').toggleClass('active_menu');
         });
 
-        if($('.page-articles .posted-mid:empty')){
-            $('.page-articles .history-left').css({'width':'100%'});
+        if($('.page-articles .posted-mid:has(img)')){
+            $('.page-articles .history-left').addClass('full');
         };
 
             $('.front .view-videos .views-row').each(function (i) {
