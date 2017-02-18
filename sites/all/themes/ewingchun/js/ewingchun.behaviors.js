0cd5db849fa259c5f0bc71ec44fab77943ed89ca
vitaliy <vetaliy80@mail.ru>
2017-02-18 14:42:33 +0200

    empty blog fixes

--- sites/all/themes/ewingchun/js/ewingchun.behaviors.js
+++ sites/all/themes/ewingchun/js/ewingchun.behaviors.js
@@ -74,7 +74,7 @@
         $('.page-wiki .view-wiki .views-submit-button input').val("Go");
 
        if($('.page-blog:has(.view-empty)')){
-            $('#block-views-exp-blogs-page-1').css({"margin-top":"0"})
+            $(this).addClass("empty")
 
         }
 
