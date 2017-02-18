4887563acfb8f9613780cc35149a4baaa0c67d72
vitaliy <vetaliy80@mail.ru>
2017-02-18 13:13:10 +0200

    empty blog fixes

--- sites/all/themes/ewingchun/js/ewingchun.behaviors.js
+++ sites/all/themes/ewingchun/js/ewingchun.behaviors.js
@@ -74,11 +74,12 @@
         $('.page-wiki .view-wiki .views-submit-button input').val("Go");
 
         if($('.page-blog .view-Blogs:not(:has(.views-row))')){
-            $('#block-views-exp-blogs-page-1').css({"margin-top":"0"}),
-                else{
-                $('#block-views-exp-blogs-page-1').css({"margin-top":"200"})
-            }
-        };
+            $('#block-views-exp-blogs-page-1').css({"margin-top":"0"})
+
+        }
+        else{
+            $('#block-views-exp-blogs-page-1').css({"margin-top":"200"})
+        }
 
 
     }
